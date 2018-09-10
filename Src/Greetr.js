91cdbc89d45c05efc3d99753cf7fdefc1c7e78d1
Pkalra7 <pritish_kalra@yahoo.co.in>
2018-09-10 13:00:21 -0400

    Library is functional now. Greetings in English, Spanish and French offered.

--- Src/Greetr.js
+++ Src/Greetr.js
@@ -4,19 +4,119 @@
 */
 
 /*we're going to imitate jquery's structure*/
-(function(global, $) {
+(function(global) {
     
     var Greetr = function(firstName, lastName, language){
         return new Greetr.init(firstName, lastName, language);
     }  
   
-    Greetr.prototype = {};
+    var supportedLangs = ['en', 'es', 'fr'];
+    
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola',
+        fr: 'Salut'
+        
+    };
+    
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos',
+        fr: 'Bonjour'
+    };
+    
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Incio sesion',
+        fr: 'connectée'
+        
+    };
+    
+    Greetr.prototype = {
+        
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+        
+        validate: function(){
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+        
+        greeting: function(){
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+        
+        formalGreeting: function(){
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+        
+        greet: function(formal){
+            var msg;
+            
+            //if undefined or null, will be coerced to false
+            if (formal) {
+                msg = this.formalGreeting();
+        
+            }else{
+                msg = this.greeting();
+            }
+            
+            if(console){
+                console.log(msg);
+            }
+            
+            //to make this method chainable, 'this' refers to the calling object at execution time
+            return this;
+        },
+        
+        log: function(){
+            if(console){
+                console.log(logMessages[this.language] + ': ' + this.fullName());
+            }
+            
+            return this;
+        },
+        
+        setLang : function(lang){
+            this.language = lang;
+            
+            this.validate();
+            
+            return this;
+        },
+        
+        HTMLGreeting: function(selector, formal){
+            if(!$) {
+                throw 'jQuery not loaded1';
+            }
+            
+            if(!selector){
+                throw 'Missing jQuery selector';
+            }
+            
+            var msg;
+            if(formal){
+                msg = this.formalGreeting();
+            } else {
+                msg = this.greeting();
+            }
+            
+            $(selector).html(msg);
+            
+            return this;
+        }
+    } 
+        
   
     Greetr.init = function(firstName, lastName, language){
+        
         var self = this;
         self.firstName = firstName || '' ;
         self.lastName = lastName || '' ;
         self.language = language || 'en' ;
+        
     }
   
     Greetr.init.prototype = Greetr.prototype;
--- Src/app.js
+++ Src/app.js
@@ -1,2 +1,18 @@
 var g = G$('John', 'Doe');
-console.log(g);
+
+g.greet(true).setLang('es').greet(true);
+
+$('#login').click(function(){
+    
+    var loginGtr = G$('John', 'Doe');
+    
+    loginGtr.setLang($('#lang').val());
+        
+    if($('input:checked').val() === "Formal"){
+            loginGtr.HTMLGreeting('#greeting', true);
+    }else{
+        loginGtr.HTMLGreeting('#greeting');
+    }
+    
+    console.log(loginGtr.log());
+});
